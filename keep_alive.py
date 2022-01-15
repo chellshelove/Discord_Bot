30e8e10c65f1d721fde39b5d39e6b3db44c9340e
chellshelove <chellshelove733@gmail.com>
2022-01-16 00:07:45 +0700

    updated comments

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask # use flask as the web server 
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hello. I am alive!"
+    return "Hello. I am alive!" # return "hello i'm alive" to amyone who visits the server 
 
 def run():
   app.run(host='0.0.0.0',port=8080)
